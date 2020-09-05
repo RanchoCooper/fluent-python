c4a1013edde1ecfd517306cbc80e396ccd294e54
Rancho <ranchocooper@gmail.com>
2020-09-05 13:10:38 +0800

    ch01: french deck

---
+++ chapter01/frenck_deck.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+    suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, index):
+        return self._cards[index]
+
+    def spades_high(card):
+        rank_value = FrenchDeck.ranks.index(card.rank)
+        return rank_value * len(FrenchDeck.suit_values) + FrenchDeck.suit_values[card.suit]
+
